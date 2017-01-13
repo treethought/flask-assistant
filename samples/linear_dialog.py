06b7e0a98da123810a29633edd21ebbba429d430
Cam Sweeney <cpsweene@gmail.com>
2017-01-12 18:23:52 -0800

    remove old samples

--- samples/linear_dialog.py
+++
@@ -1,9 +0,0 @@
-import logging
-from flask import Flask, request, Response, jsonify, json, make_response
-from flask_assistant import Assistant, statement
-
-app = Flask(__name__)
-assistant = Assistant(app)
-logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
-
-@assistant.action()
