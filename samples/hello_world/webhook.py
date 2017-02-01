f875755c3cf617c74004873d34c735b2eb27e045
Cam Sweeney <cpsweene@gmail.com>
2017-02-01 12:41:32 -0800

    hello_word example from quickstart

---
+++ samples/hello_world/webhook.py
@@ -0,0 +1,35 @@
+import logging
+from flask import Flask
+from flask_assistant import Assistant, ask, tell, context_manager
+
+app = Flask(__name__)
+assist = Assistant(app)
+logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
+
+
+@assist.action('greeting')
+def greet_and_start():
+    speech = "Hey! Are you male or female?"
+    return ask(speech)
+
+
+@assist.action("user-gives-gender")
+def ask_for_color(gender):
+    if gender == 'male':
+        gender_msg = 'Sup bro!'
+
+    else:
+        gender_msg = 'Haay gurl!'
+
+    speech = gender_msg + ' What is your favorite color?'
+    return ask(speech)
+
+
+@assist.action('give-color', mapping={'color': 'sys.color'})
+def repeat_color(color):
+    speech = 'Ok, {} is an okay color I guess.'.format(color)
+    return ask(speech)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
