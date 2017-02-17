81244bacef15e8f4b6bee219f9a4fe0b0b15a35f
Cam Sweeney <cpsweene@gmail.com>
2017-02-16 17:55:34 -0800

    fixed typo

--- flask_assistant/__init__.py
+++ flask_assistant/__init__.py
@@ -8,7 +8,7 @@ if logger.level == logging.NOTSET:
 
 from flask_assistant.core import (
     Assistant,
-    context_manager,
+    context_manager
 )
 
 from flask_assistant.response import ask, tell
