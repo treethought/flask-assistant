ff472b8b99344915c6e9ec789eb3959de4f15924
Cam Sweeney <cpsweene@gmail.com>
2017-03-13 16:49:07 -0700

    event import

--- flask_assistant/__init__.py
+++ flask_assistant/__init__.py
@@ -11,5 +11,5 @@ from flask_assistant.core import (
     context_manager
 )
 
-from flask_assistant.response import ask, tell
+from flask_assistant.response import ask, tell, event
 from flask_assistant.manager import Context
