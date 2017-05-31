3c82f4039eb9bc8064d7f879c2f40a3db49f8680
Cam Sweeney <cpsweene@gmail.com>
2017-05-31 02:01:18 -0700

    version bump 0.2.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Assistant',
-    version='0.2.6',
+    version='0.2.7',
     url='https://github.com/treethought/flask-assistant',
     license='Apache 2.0',
     author='Cam Sweeney',
