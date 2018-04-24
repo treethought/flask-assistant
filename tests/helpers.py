03bf240b011c54f1e4d3b5b5a6ca15cfe2d6b92a
Georg Molau <g.molau@campus.tu-berlin.de>
2018-04-24 15:27:43 +0200

    Add dummy 'originalRequest' object to test payload
    Fixes a test failure with the access_token proxy. 'originalRequest' is
    data passed by Google Actions to Dialogflow and is included in every
    request from Dialogflow to the fulfillment endpoint.

--- tests/helpers.py
+++ tests/helpers.py
@@ -3,6 +3,55 @@ import json
 def build_payload(intent, params={}, contexts=[], action='test_action', query='test query'):
 
     return json.dumps({
+        "originalRequest": {
+            "source": "google",
+            "version": "2",
+            "data": {
+                "isInSandbox": False,
+                "surface": {
+                    "capabilities": [
+                        {
+                            "name": "actions.capability.AUDIO_OUTPUT"
+                        },
+                        {
+                            "name": "actions.capability.SCREEN_OUTPUT"
+                        },
+                        {
+                            "name": "actions.capability.MEDIA_RESPONSE_AUDIO"
+                        },
+                        {
+                            "name": "actions.capability.WEB_BROWSER"
+                        }
+                    ]
+                },
+                "inputs": [
+                    {}
+                ],
+                "user": {
+                    "lastSeen": "2018-04-23T15:10:43Z",
+                    "locale": "en-US",
+                    "userId": "abcdefg",
+                    "accessToken": "foobar"
+                },
+                "conversation": {
+                    "conversationId": "123456789",
+                    "type": "ACTIVE",
+                    "conversationToken": "[]"
+                },
+                "availableSurfaces": [
+                    {
+                        "capabilities": [
+                            {
+                                "name": "actions.capability.AUDIO_OUTPUT"
+                            },
+                            {
+                                "name": "actions.capability.SCREEN_OUTPUT"
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
         "id": "8ea2d357-10c0-40d1-b1dc-e109cd714f67",
         "timestamp": "2017-06-26T22:43:14.935Z",
         "lang": "en",
