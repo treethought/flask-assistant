bc2905bce76597705afcbd230a8e75ec0f06c55c
treethought <cpsweene@gmail.com>
2018-12-16 20:21:40 -0800

    add actions_on_google integration config

--- samples/hello_world/webhook.py
+++ samples/hello_world/webhook.py
@@ -1,35 +1,67 @@
 import logging
 from flask import Flask
-from flask_assistant import Assistant, ask, tell, context_manager
+from flask_assistant import Assistant, ask, tell, context_manager, permission, event
 
 app = Flask(__name__)
 assist = Assistant(app)
-logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
+logging.getLogger("flask_assistant").setLevel(logging.DEBUG)
+
+app.config["INTEGRATIONS"] = ["ACTIONS_ON_GOOGLE"]
 
 
-@assist.action('greeting')
+@assist.action("greeting")
 def greet_and_start():
     speech = "Hey! Are you male or female?"
-    return ask(speech)
+    resp = ask(speech)
+    resp.suggest("Male", "Female")
+    return resp
+
+
+@assist.prompt_for("gender", "give-gender")
+def prompt_gender(gender):
+    return ask("I need to know your gender")
 
 
 @assist.action("give-gender")
 def ask_for_color(gender):
-    if gender == 'male':
-        gender_msg = 'Sup bro!'
+    if gender.lower() == "male":
+        gender_msg = "Sup bro!"
 
     else:
-        gender_msg = 'Haay gurl!'
+        gender_msg = "Haay gurl!"
 
-    speech = gender_msg + ' What is your favorite color?'
+    speech = gender_msg + " What is your favorite color?"
     return ask(speech)
 
 
-@assist.action('give-color', mapping={'color': 'sys.color'})
+@assist.prompt_for("color", intent_name="give-color")
+def prompt_color(color):
+    speech = "Sorry I didn't catch that. What color did you say?"
+    return ask(speech)
+
+
+@assist.action("give-color", mapping={"color": "sys.color"})
 def repeat_color(color):
-    speech = 'Ok, {} is an okay color I guess.'.format(color)
+    speech = "Ok, {} is an okay color I guess.".format(color)
+    speech += "\n What pizza do you want"
     return ask(speech)
 
 
-if __name__ == '__main__':
+@assist.action("pizza")
+def get_pizza(size, pizza_type):
+    speech = f"Cool, I'll make you a {size} {pizza_type} pizza"
+    return ask(speech)
+
+
+@assist.prompt_for("size", "pizza")
+def prompt_size(size):
+    return ask("I need to know what size pizza")
+
+
+@assist.prompt_for("pizza_type", "pizza")
+def prompt_pizza_type(pizza_type):
+    return ask("What kind of pizza?")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
