a6295ea3061b99410651ff7f8562957f937ee243
treethought <cpsweene@gmail.com>
2018-12-16 19:30:37 -0800

    update tests for new request/response format

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,90 +1,70 @@
 import json
 
-def build_payload(intent, params={}, contexts=[], action='test_action', query='test query'):
 
-    return json.dumps({
-        "originalRequest": {
-            "source": "google",
-            "version": "2",
-            "data": {
-                "isInSandbox": False,
-                "surface": {
-                    "capabilities": [
-                        {
-                            "name": "actions.capability.AUDIO_OUTPUT"
-                        },
-                        {
-                            "name": "actions.capability.SCREEN_OUTPUT"
-                        },
-                        {
-                            "name": "actions.capability.MEDIA_RESPONSE_AUDIO"
-                        },
-                        {
-                            "name": "actions.capability.WEB_BROWSER"
-                        }
-                    ]
-                },
-                "inputs": [
-                    {}
-                ],
-                "user": {
-                    "lastSeen": "2018-04-23T15:10:43Z",
-                    "locale": "en-US",
-                    "userId": "abcdefg",
-                    "accessToken": "foobar"
-                },
-                "conversation": {
-                    "conversationId": "123456789",
-                    "type": "ACTIVE",
-                    "conversationToken": "[]"
-                },
-                "availableSurfaces": [
-                    {
+def build_payload(
+    intent, params={}, contexts=[], action="test_action", query="test query"
+):
+
+    return json.dumps(
+        {
+            "originalDetectIntentRequest": {
+                "source": "google",
+                "version": "2",
+                "data": {
+                    "isInSandbox": False,
+                    "surface": {
                         "capabilities": [
-                            {
-                                "name": "actions.capability.AUDIO_OUTPUT"
-                            },
-                            {
-                                "name": "actions.capability.SCREEN_OUTPUT"
-                            }
+                            {"name": "actions.capability.AUDIO_OUTPUT"},
+                            {"name": "actions.capability.SCREEN_OUTPUT"},
+                            {"name": "actions.capability.MEDIA_RESPONSE_AUDIO"},
+                            {"name": "actions.capability.WEB_BROWSER"},
                         ]
-                    }
-                ]
-            }
-        },
-        "id": "8ea2d357-10c0-40d1-b1dc-e109cd714f67",
-        "timestamp": "2017-06-26T22:43:14.935Z",
-        "lang": "en",
-        "result": {
-            "action": action,
-            "actionIncomplete": False,
-            "contexts": contexts,
-            "fulfillment": {
-                "messages": [],
-                "speech": ""
+                    },
+                    "inputs": [{}],
+                    "user": {
+                        "lastSeen": "2018-04-23T15:10:43Z",
+                        "locale": "en-US",
+                        "userId": "abcdefg",
+                        "accessToken": "foobar",
+                    },
+                    "conversation": {
+                        "conversationId": "123456789",
+                        "type": "ACTIVE",
+                        "conversationToken": "[]",
+                    },
+                    "availableSurfaces": [
+                        {
+                            "capabilities": [
+                                {"name": "actions.capability.AUDIO_OUTPUT"},
+                                {"name": "actions.capability.SCREEN_OUTPUT"},
+                            ]
+                        }
+                    ],
+                },
             },
-            "metadata": {
-                "intentId": "some-intent-id",
-                "intentName": intent,
-                "webhookForSlotFillingUsed": "false",
-                "webhookUsed": "true"
+            "responseId": "8ea2d357-10c0-40d1-b1dc-e109cd714f67",
+            "queryResult": {
+                "action": action,
+                "allRequiredParamsCollected": True,
+                "outputContexts": contexts,
+                "languageCode": "en",
+                "fulfillment": {"messages": [], "text": ""},
+                "intent": {
+                    "name": "some-intent-id",
+                    "displayName": intent,
+                    # "webhookForSlotFillingUsed": "false",
+                    "webhookState": True,
+                },
+                "parameters": params,
+                "resolvedQuery": query,
+                "intentDetectionConfidence": 1.0,
             },
-            "parameters": params,
-            "resolvedQuery": query,
-            "score": 1.0,
-            "source": "agent",
-            "speech": ""
-        },
-        "status": {
-            "code": 200,
-            "errorType": "success"
-        },
-        "sessionId": "c24d9cfe-21c9-4fc0-a5eb-1a2ee1fec29c"
-    })
-
+            "session": "projects/test-project-id/agent/sessions/88d13aa8-2999-4f71-b233-39cbf3a824a0",
+        }
+    )
 
 
 def get_query_response(client, payload):
-    resp = client.post('/', data=payload)
+    resp = client.post("/", data=payload)
     assert resp.status_code == 200
-    return json.loads(resp.data.decode('utf-8'))
+    return json.loads(resp.data.decode("utf-8"))
--- tests/test_samples.py
+++ tests/test_samples.py
@@ -2,44 +2,24 @@ from flask_assistant.utils import get_assistant
 from tests.helpers import build_payload, get_query_response
 
 
-
-
 def test_hello_world_greeting(hello_world_assist):
     client = hello_world_assist.app.test_client()
-    payload = build_payload('greeting')
+    payload = build_payload("greeting")
     resp = get_query_response(client, payload)
-    assert 'male or female' in resp['speech']
+    assert "male or female" in resp["fulfillmentText"]
+
 
 def test_hello_world_give_gender(hello_world_assist):
     client = hello_world_assist.app.test_client()
-    payload = build_payload('give-gender', params={'gender': 'male'})
+    payload = build_payload("give-gender", params={"gender": "male"})
     resp = get_query_response(client, payload)
-    assert 'Sup bro' in resp['speech']
-    assert 'What is your favorite color?' in resp['speech']
+    assert "Sup bro" in resp["fulfillmentText"]
+    assert "What is your favorite color?" in resp["fulfillmentText"]
+
 
 def test_hello_world_give_color(hello_world_assist):
     client = hello_world_assist.app.test_client()
-    payload = build_payload('give-color', params={'color': 'blue'})
+    payload = build_payload("give-color", params={"color": "blue"})
     resp = get_query_response(client, payload)
 
-    assert 'Ok, blue is an okay' in resp['speech']
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    assert "Ok, blue is an okay" in resp["fulfillmentText"]
