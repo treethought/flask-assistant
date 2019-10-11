2211e08462a4202e5d04aa68a6c854710937fa46
treethought <cpsweene@gmail.com>
2019-10-11 12:59:51 -0700

    version bump v0.4.1 - update dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("./README.md", "r") as f:
 
 setup(
     name="Flask-Assistant",
-    version="0.4.0",
+    version="0.4.1",
     url="https://github.com/treethought/flask-assistant",
     license="Apache 2.0",
     author="Cam Sweeney",
